17e1652aa2eb8853965075b0cf6d52fc6f19ff09
fionahevans <fiona.h.evans@gmail.com>
2026-04-08 13:02:07 +0800

    Bayswater event

--- content/the_track/banner.R
+++ content/the_track/banner.R
@@ -7,14 +7,28 @@ img1 <- image_read("cover.jpg")
 img2 <- image_read("kalamunda.jpg")
 img3 <- image_read("collie.png")
 img4 <- image_read("dwellingup.png")
+img5 <- image_read("albany.jpg")
 
 # Combine images horizontally (stack = FALSE)
 combined_h <- image_append(c(image_resize(img1, "x300"), 
-                             image_resize(img2, "x300"),
                              image_resize(img3, "x300"),
-                             image_resize(img4, "x300")), 
+                             image_resize(img4, "x300"),
+                             image_resize(img5, "x300")), 
                            stack = FALSE)
 
 # Display and save
 print(combined_h)
 image_write(combined_h, "combined_image.jpg")
+
+
+
+# Combine images for Substack header
+combined_w <- image_append(c(image_resize(img1, "x401"), 
+                             image_resize(img3, "x401"),
+                             image_resize(img4, "x401"),
+                             image_resize(img5, "x401")), 
+                           stack = FALSE)
+
+# Display and save
+print(combined_w)
+image_write(combined_w, "combined_image2.jpg")
