b2c60346894b3ee5a88c8458a4c1b1a6e98f6d14
fionahevans <fiona.h.evans@gmail.com>
2026-04-26 12:57:44 +0800

    walpole poster

--- content/the_track/banner.R
+++ content/the_track/banner.R
@@ -8,12 +8,14 @@ img2 <- image_read("kalamunda.jpg")
 img3 <- image_read("collie.png")
 img4 <- image_read("dwellingup.png")
 img5 <- image_read("albany.jpg")
+img6 <- image_read("denmark.jpg")
+img7 <- image_read("walpole.jpg")
 
 # Combine images horizontally (stack = FALSE)
 combined_h <- image_append(c(image_resize(img1, "x300"), 
-                             image_resize(img3, "x300"),
-                             image_resize(img4, "x300"),
-                             image_resize(img5, "x300")), 
+                             image_resize(img5, "x300"),
+                             image_resize(img6, "x300"),
+                             image_resize(img7, "x300")), 
                            stack = FALSE)
 
 # Display and save
